175be1bb8b0de4714baee40ca9566982fd54e7c4
Volodymyr Shevchuk <dosandkv@gmail.com>
2020-09-09 22:55:27 +0300

    Finished project;

---
+++ README.md
@@ -0,0 +1,12 @@
+# Project Structure
+
+## To start project in development mode:
+
+`npm install` - установит необходимые зависимости
+`npm run develop` - запустит "WebpackDevServer"
+
+**Note:** Версии требуемые проектом указаны в `package.json` в поле `engines` 
+
+## To build project:
+
+`npm run build` - "соберет" проект в "prodcution" режиме
---
+++ src/index.js
@@ -0,0 +1,25 @@
+import Router from './router/index.js';
+import tooltip from './components/tooltip/index.js';
+
+tooltip.initialize();
+
+const router = Router.instance();
+
+router
+  .addRoute(/^$/, 'dashboard')
+  .addRoute(/^products$/, 'products/list')
+  .addRoute(/^products\/add$/, 'products/edit')
+  .addRoute(/^products\/([\w()-]+)$/, 'products/edit')
+  .addRoute(/^sales$/, 'sales')
+  .addRoute(/^categories$/, 'categories')
+  .addRoute(/^404\/?$/, 'error404')
+  .setNotFoundPagePath('error404')
+  .listen();
+
+const toggleSidebar = (e) => {
+  e.preventDefault();
+  document.body.classList.toggle("is-collapsed-sidebar");
+}
+
+const sidebarToggler = document.body.querySelector(".sidebar__toggler");
+sidebarToggler.addEventListener("click", toggleSidebar);
